848b6277c1429db8f072b7bd6f4418ef492fed5e
SimonSAMPERE <41120579+SimonSAMPERE@users.noreply.github.com>
2019-04-15 16:18:02 +0200

    connection and query to the API with a class - simplify #16

--- api/request_api_module.py
+++ api/request_api_module.py
@@ -9,46 +9,28 @@
 import json 
 
 #Package
-from isogeo_pysdk import Isogeo, IsogeoUtils, __version__
+from isogeo_pysdk import Isogeo, IsogeoUtils
 utils = IsogeoUtils()
 
 # ############################################################################
-# ########## FUNCTIONS #############
+# ########## Classe #############
 # ##################################
-
-# API authentification with json file
-def API_auth(jsonFile):
-     api_credentials = utils.credentials_loader(jsonFile)
-     isogeo = Isogeo(client_id=api_credentials.get("client_id"), client_secret=api_credentials.get("client_secret"))
-     token = isogeo.connect()
-
-     return isogeo, token
-
-# Make request to the API
-def API_request_Maker(auth, filter_query = "", request_all = 1):
-     if request_all == 1:
-          search = auth[0].search(auth[1], whole_share = 0, page_size = 0, augment=0)
-     else :
-          search = auth[0].search(auth[1], whole_share = 0, page_size = 0, augment=0, query = filter_query)
-
-     return search
-
-# Write the result of the request in a json file
-def json_request_Writer(search, title):
-     with open("{}.json".format(title), "w") as json_basic:
-          json.dump(search,
-                         json_basic,
-                         sort_keys=True,
-                         indent=4,
-                         )
-
-# Extract filters' values from an API request in a meta dictionnary
-def get_Filters_Values(search):
-     dict_Filters_Values = {"keyword":{}, "owner":{}, "type":{}, "format":{}, "provider":{}}
-     if search.get("total") > 0:
-          all_tags = search.get("tags")
-          for tag in all_tags:
-               if tag.split(":")[0] in dict_Filters_Values:
-                    dict_Filters_Values[tag.split(":")[0]][tag]=all_tags[tag]
-
-     return dict_Filters_Values
+class isogeo_API(Isogeo): #Une classe qui hérite de la classe Isogeo
+     def __init__(self, file_name):
+          # création du chemin vers le fichier d'authentification
+          self.json_file = "{}/{}".format("/".join(__file__.split("\\")[:-1]), file_name)
+          # récupérétion des informations d'authentification contenues dans le fichier
+          self.client_id = utils.credentials_loader(self.json_file).get("client_id")
+          self.client_secret = utils.credentials_loader(self.json_file).get("client_secret")
+          # connexion à l'API et récupération du token 
+          self.isogeo = Isogeo(client_id = self.client_id, client_secret = self.client_secret)
+          self.token = self.isogeo.connect()
+     
+     # Méthode permettant d'effectuer les différents types de requête à l'API
+     def request_Maker(self, filter_request = 1, filter_query = ""):
+          if filter_request == 1:
+               search = self.isogeo.search(self.token, whole_share = 0, page_size = 0, augment=0, tags_as_dicts = 1, query = filter_query)
+          else :
+               search = self.isogeo.search(self.token, whole_share = 0, page_size = 0, augment=0, tags_as_dicts = 1)
+          # retourne les valeurs des champs ainsi que le nombre de métadonnées filtrées
+          return search.get("tags"), search.get("total") 
